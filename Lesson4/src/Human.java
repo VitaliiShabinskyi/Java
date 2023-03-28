5b64f9e58978546ed16f146c51fca9b6d598e181
Vitalii.Shabinskyi <vetalsoft@gmail.com>
2023-03-28 09:13:15 +0200

    Lesson 4 (27.03)

---
+++ Lesson4/src/Human.java
@@ -0,0 +1,17 @@
+public class Human {
+    String name;
+    String position;
+
+    public Human(String name, String position) {                      // Конструктор
+        this.name = name;
+        this.position = position;
+    }
+
+    public void out(){
+        System.out.println("Name : " + name +
+                " Position : " + position);
+    }
+
+
+}
+
